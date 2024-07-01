ecf5fa3636cfbd0c91cf4fd78fd8429520dd2ff1
IsmaelT <75395667+Techanis@users.noreply.github.com>
2024-07-01 15:26:23 +0700

    Add getComputerChoice function
    Function returns the computer choice (Rock, paper or scissor)

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log("Prueba");
+// console.log("Prueba");
+
+function getComputerChoice() {
+    let i = Math.floor(Math.random()*3);
+    let choice = "";
+    switch (i){
+        case 0:
+            choice = "rock";
+            break;
+        case 1:
+            choice = "paper"
+            break;
+        case 2:
+            choice = "scissor"
+            break;
+        default:
+            return "Error"
+    }
+
+    return choice;
+}
+
+console.log(getComputerChoice());
