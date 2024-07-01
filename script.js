f702b66749ed66550766653ecdd28cb2b7d92adc
IsmaelT <75395667+Techanis@users.noreply.github.com>
2024-07-01 22:30:14 +0700

    Finish example
    Added for loop for repeat the game 5 times, and at the end a winner is
    chosen

--- script.js
+++ script.js
@@ -33,8 +33,8 @@ function getHumanChoice() {
 
 // getHumanChoice();
 
-let humanChoice = getHumanChoice();
-let computerChoice = getComputerChoice();
+// let humanChoice = getHumanChoice();
+// let computerChoice = getComputerChoice();
 
 function playRound(humanChoice, computerChoice){
     // 1 = win, 2 = lose, 3 = tie
@@ -48,7 +48,7 @@ function playRound(humanChoice, computerChoice){
     } else {
         result = 2;
     }         
-    console.log(result);
+    // console.log(result);
     switch (result){
         case 1 :
             console.log(`You win!! ${humanChoice} beats ${computerChoice}`);
@@ -60,7 +60,33 @@ function playRound(humanChoice, computerChoice){
             console.log(`It's a tie!! You both choose ${computerChoice}`);
             break;
     }
+    return result;
 }
 
 
-playRound(humanChoice, computerChoice);
+// playRound(humanChoice, computerChoice);
+alert("Welcome to Rock-paper-scissors game!")
+
+function playGame(){
+    let result;
+    for (let i =0 ; i < 5; i++) {
+        
+        let humanChoice = getHumanChoice().toLowerCase();
+        let computerChoice = getComputerChoice();
+         // 1 = win, 2 = lose, 3 = tie
+        result = playRound(humanChoice, computerChoice);
+        if (result === 1) humanScore++;
+        else if (result === 2) computerScore++;
+        console.log(`Score --> human: ${humanScore}, computer: ${computerScore}`)
+    }
+    if (humanScore>computerScore) {
+        console.log("The winner is human!");
+    } else if (computerScore>humanScore) {
+        console.log("The winner is computer!");
+    } else {
+        console.log("It's a tie!!");
+
+    }
+}
+
+playGame();
